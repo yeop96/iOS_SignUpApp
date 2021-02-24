269c55c8fd12427a95dfb5aa014641435c6674e8
yeop96 <ekdh6@naver.com>
2021-02-25 02:07:01 +0900

    스택 만들기

---
+++ SignUp/SecondViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SecondViewController.swift
+//  SignUp
+//
+//  Created by yeop on 2021/02/25.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func popToPreview() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
