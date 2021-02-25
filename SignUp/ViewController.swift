86d1e57ce3674e726f4b096ba5f7c9eb85897ae6
yeop96 <ekdh6@naver.com>
2021-02-26 00:52:06 +0900

    타겟-액션 디자인 패턴 적용한 데이트 픽커, 제스쳐 인식기 적용

--- SignUp/ViewController.swift
+++ SignUp/ViewController.swift
@@ -13,12 +13,36 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var ageField: UITextField!
+    
+    @IBOutlet weak var datePickerL: UIDatePicker! // preferred style : Automatic 이 아닌 wheels로 변경
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    let dateFormatter: DateFormatter = {
+        let formatter: DateFormatter = DateFormatter()
+        
+        formatter.dateFormat = "yyyy/MM/dd hh:mm:ss"
+        
+        //formatter.dateStyle = .medium
+        //formatter.timeStyle = .medium
+        
+        return formatter
+    }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("ViewController view 가 메모리에 로드 됨")
+        //코드로 타겟 설정 후 액션 연결
+        self.datePickerL.addTarget(self, action: #selector(self.didDatePickerValueChanged(_:)), for: UIControl.Event.valueChanged)
+        
+        // 탭 제스쳐 코드로 타겟 액션 추가
+        //let tabGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.tapView(_:)))
+        //self.view.addGestureRecognizer(tabGesture)
+        
+        
+        //print("ViewController view 가 메모리에 로드 됨")
     }
     
     lazy var imagePicker: UIImagePickerController = {
@@ -50,41 +74,52 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         UserInformation.shared.age = ageField.text
     }
     
+    @IBAction func didDatePickerValueChanged(_ sender: UIDatePicker){
+        let date: Date = self.datePickerL.date
+        let dateString: String = self.dateFormatter.string(from: date)
+        
+        self.dateLabel.text = dateString
+    }
+    
+    @IBAction func tapView(_ sender: UITapGestureRecognizer){
+        self.view.endEditing(true)
+    }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        print("ViewController view 가 화면에 보여질 예정")
+       // print("ViewController view 가 화면에 보여질 예정")
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        print("ViewController view 가 화면에 보여짐")
+        // print("ViewController view 가 화면에 보여짐")
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        print("ViewController view 가 화면에 사라질 예정")
+        // print("ViewController view 가 화면에 사라질 예정")
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         
-        print("ViewController view 가 화면에 사라짐")
+        // print("ViewController view 가 화면에 사라짐")
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         
-        print("ViewController view 가 subview를 레이아웃 하려함")
+        // print("ViewController view 가 subview를 레이아웃 하려함")
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-        print("ViewController view 가 subview를 레이아웃 함")
+        // print("ViewController view 가 subview를 레이아웃 함")
     }
     
     
